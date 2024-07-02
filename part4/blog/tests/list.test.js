4a5b5c48274e84cb54db8f6631d1cd95fdd72a70
Tyler Eck <tylereck81@yahoo.com>
2024-07-02 14:50:57 +0300

    Finished 4.5 Unit Tests

--- part4/blog/tests/list.test.js
+++ part4/blog/tests/list.test.js
@@ -53,7 +53,7 @@ const listWithMultipleBlogs = [
       title: "TDD harms architecture",
       author: "Robert C. Martin",
       url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
-      likes: 0,
+      likes: 12,
       __v: 0
     },
     {
@@ -75,7 +75,7 @@ test('dummy returns one', () => {
 })
 
 
-describe('total likes', () => {
+describe('total likes of all blogs', () => {
     test('when list has only one blog, equals the likes of that', () => {
       const result = listHelper.totalLikes(listWithOneBlog)
       assert.strictEqual(result, 5)
@@ -88,6 +88,44 @@ describe('total likes', () => {
 
     test('when list has multiple blogs', () => {
         const result = listHelper.totalLikes(listWithMultipleBlogs)
-        assert.strictEqual(result, 36)
+        assert.strictEqual(result, 48)
     })
+})
+
+
+describe('most liked blog', () => {
+    test('empty blogs', () => {
+        const result = listHelper.favoriteBlog(emptyBlog)
+        assert.deepStrictEqual(result, {})
+    })
+
+    test('when list has one blog', () => {
+        const result = listHelper.favoriteBlog(listWithOneBlog)
+        assert.deepStrictEqual(result,
+            {
+                _id: '5a422aa71b54a676234d17f8',
+                title: 'Go To Statement Considered Harmful',
+                author: 'Edsger W. Dijkstra',
+                url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+                likes: 5,
+                __v: 0
+            }
+        )
+    })
+
+    test('when list has multiple blogs', () => {
+        const result = listHelper.favoriteBlog(listWithMultipleBlogs)
+        assert.deepStrictEqual(result,
+            {
+                _id: "5a422b3a1b54a676234d17f9",
+                title: "Canonical string reduction",
+                author: "Edsger W. Dijkstra",
+                url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+                likes: 12,
+                __v: 0
+            }
+        )
+    })
+
+
 })
--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -7,7 +7,23 @@ const totalLikes = (blogs) => {
   return blogs.reduce((total, blog) => total + blog.likes, 0)
 } 
 
+const favoriteBlog = (blogs) => {
+  let max = -1000;
+  let maxLikedBlog = {};
+
+  for (const b of blogs) {
+
+    if (b.likes > max) {
+      max = b.likes;
+      maxLikedBlog = b;
+
+    }
+  }
+  return maxLikedBlog;
+}
+
 module.exports = {
   dummy, 
-  totalLikes
+  totalLikes, 
+  favoriteBlog
 }
