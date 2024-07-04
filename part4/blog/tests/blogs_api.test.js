2998f02f39988847b36641a5fbddd2f23e42ebdf
Tyler Eck <tylereck81@yahoo.com>
2024-07-04 17:35:01 +0300

    Finished 4.10 Blog List Tests

--- part4/blog/tests/blogs_api.test.js
+++ part4/blog/tests/blogs_api.test.js
@@ -41,73 +41,46 @@ describe('when there is initially some blogs saved', () =>{
 
 })
 
+describe('when a new blog is added', () =>{ 
 
+    test('suceeds with valid blog', async() =>{ 
+        const newBlog = {
+            title: "Valid Blog",
+            author: "Tyler Eck",
+            url: "http://tylervalidblog.com",
+            likes: 33,
+        }
 
-// test('a valid note can be added ', async () => {
-//   const newNote = {
-//     content: 'async/await simplifies making async calls',
-//     important: true,
-//   }
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+    
+        const blogsAtEnd = await helper.blogInDb()
+        assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
 
-//   await api
-//     .post('/api/notes')
-//     .send(newNote)
-//     .expect(201)
-//     .expect('Content-Type', /application\/json/)
+        const titles = blogsAtEnd.map(n => n.title)
+        assert(titles.includes('Valid Blog'))
+    })
 
-//   const notesAtEnd = await helper.notesInDb()
-//   assert.strictEqual(notesAtEnd.length, helper.initialNotes.length+1)
+    test('fails with status code 400 for invalid blog', async() =>{ 
+        const missingAuthor = {
+            title: "Invalid Blog",
+            url: "ahajdshkf",
+            likes: 50
+        }
 
-//   const contents = notesAtEnd.map(n => n.content)
-//   assert(contents.includes('async/await simplifies making async calls'))
-// })
+        await api
+            .post('/api/blogs')
+            .send(missingAuthor)
+            .expect(400)
+    
+        const blogsAtEnd = await helper.blogInDb()
+        assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length)
+    })
 
-// test('note without content is not added', async () => {
-//   const newNote = {
-//     important: true
-//   }
-
-//   await api
-//     .post('/api/notes')
-//     .send(newNote)
-//     .expect(400)
-
-//   const notesAtEnd = await helper.notesInDb()
-
-//   assert.strictEqual(notesAtEnd.length, helper.initialNotes.length)
-// })
-
-// test('a specific note can be viewed', async () => {
-//   const notesAtStart = await helper.notesInDb()
-
-//   const noteToView = notesAtStart[0]
-
-//   const resultNote = await api
-//     .get(`/api/notes/${noteToView.id}`)
-//     .expect(200)
-//     .expect('Content-Type', /application\/json/)
-
-//   assert.deepStrictEqual(resultNote.body, noteToView)
-//   //assert.strictEqual(JSON.stringify(resultNote.body.toString()), JSON.stringify(noteToView.toString()))
-// })
-
-// test('a note can be deleted', async () => {
-//   const notesAtStart = await helper.notesInDb()
-//   const noteToDelete = notesAtStart[0]
-
-//   await api
-//     .delete(`/api/notes/${noteToDelete.id}`)
-//     .expect(204)
-
-//   const notesAtEnd = await helper.notesInDb()
-
-//   const contents = notesAtEnd.map(r => r.content)
-//   assert(!contents.includes(noteToDelete.content))
-
-//   assert.strictEqual(notesAtEnd.length, helper.initialNotes.length - 1)
-// })
-
-
+})
 
 after(async () => {
   await mongoose.connection.close()
