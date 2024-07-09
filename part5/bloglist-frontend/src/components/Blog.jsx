1d6e0f8e1976fe4545dbe959b80eae3e2f57a5ec
Tyler Eck <tylereck81@yahoo.com>
2024-07-09 15:52:13 +0300

    Finished Blog List Frontend 5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,47 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  
+  const [blogDetailsVisible, setBlogDetailsVisible] = useState(false)
+
+  const toggleDetails = () => {
+    setBlogDetailsVisible(!blogDetailsVisible)
+  }
+
+  const updateLikes = () => { 
+    console.log("updated likes")
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return(
+    <div className='blog' style={blogStyle}>
+      <p>
+        {blog.title} {blog.author}
+        <button onClick={toggleDetails}>{blogDetailsVisible ? 'hide' : 'view'}</button>
+      </p>
+      {blogDetailsVisible && (
+        <div>
+          <div> 
+            {blog.url}
+          </div>
+          <div>
+            likes: {blog.likes}
+            <button onClick={updateLikes}>like</button>
+          </div>
+          <div>
+            {blog.user.name}
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
 
 export default Blog
