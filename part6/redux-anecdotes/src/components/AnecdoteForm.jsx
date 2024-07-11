9d44da4fa2c6075cd7eeab0696ba868c98e36a3f
Tyler Eck <tylereck81@yahoo.com>
2024-07-11 09:17:07 +0300

    Finished Anecdotes 6.7 and 6.8

---
+++ part6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -0,0 +1,26 @@
+/* eslint-disable react/prop-types */
+import { addAnecdote } from '../reducers/anecdoteReducer'
+import { useDispatch } from 'react-redux'
+
+const AnecdoteForm = () => {
+    const dispatch = useDispatch()
+
+    const createAnecdote = async (event) => {
+      event.preventDefault()
+      const content = event.target.newAnecdote.value
+      event.target.newAnecdote.value = ''
+      dispatch(addAnecdote(content))
+    }
+
+    return (
+      <div>
+        <h2>create new</h2>
+        <form onSubmit={createAnecdote}>
+          <div><input name='newAnecdote'/></div>
+          <button type='submit'>create</button>
+        </form>
+      </div>
+    )
+}
+  
+export default AnecdoteForm
---
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -0,0 +1,32 @@
+/* eslint-disable react/prop-types */
+import { useSelector, useDispatch } from 'react-redux'
+import { addVotes } from '../reducers/anecdoteReducer'
+
+const AnecdoteList = () => {
+  const anecdotes = useSelector(state => state)
+  const dispatch = useDispatch()
+
+  const vote = (id) => {
+    dispatch(addVotes(id))
+  }
+    
+  return (
+    <div>
+        <h2>Anecdotes</h2>
+        {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+            <div>
+            {anecdote.content}
+            </div>
+            <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+            </div>
+        </div>
+        )}
+    </div>
+  )
+
+}
+  
+export default AnecdoteList
